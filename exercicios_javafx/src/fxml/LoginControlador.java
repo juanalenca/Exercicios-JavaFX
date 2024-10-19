2fda931c6b89e01b212ec8a7c1ee082de94a694a
juanalenca <juanbarros.dev@gmail.com>
2024-10-19 14:38:44 -0300

    Atualizar classpath para incluir biblioteca ControlsFX

--- exercicios_javafx/src/fxml/LoginControlador.java
+++ exercicios_javafx/src/fxml/LoginControlador.java
@@ -1,6 +1,9 @@
 package fxml;
 
+import org.controlsfx.control.Notifications;
+
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
@@ -14,8 +17,27 @@ public class LoginControlador {
 	
 	
 	public void entrar() {
+		
+		boolean emailValido = campoEmail.getText().equals("juanbarrosdev@gmail.com");
+		boolean senhaValida = campoEmail.getText().equals("123456");
+		
+		
 		System.out.println(campoEmail.getText());
 		System.out.println(campoSenha.getText());
+		
+		if(emailValido && senhaValida) {
+			Notifications.create()
+				.position(Pos.TOP_CENTER)
+				.title("Login FXML")
+				.text("Login efetuado com sucesso!")
+				.showInformation();
+		}else {
+			Notifications.create()
+				.position(Pos.TOP_CENTER)
+				.title("Login FXML")
+				.text("Usuário ou senha inválidos!")
+				.showError();
+		}
 	}
 
 }
