812649458fe8a83fe2943635d280bf0388cac5f4
juanalenca <juanbarros.dev@gmail.com>
2024-10-19 13:36:46 -0300

    Carregar e aplicar arquivo CSS no layout de login

--- exercicios_javafx/src/fxml/AppFXML.java
+++ exercicios_javafx/src/fxml/AppFXML.java
@@ -13,11 +13,14 @@ public class AppFXML extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		
+		String arquivoCSS = getClass().getResource("/fxml/Login.css").toExternalForm();
+		
 		URL arquivoFXML = getClass().getResource("/fxml/Login.fxml");
 		
 		GridPane raiz = FXMLLoader.load(arquivoFXML);
 		
 		Scene cena = new Scene(raiz, 350, 400);
+		cena.getStylesheets().add(arquivoCSS);
 		 
 		//setResizable: permição para o user altere o tamanho da tela
 		primaryStage.setResizable(false);
