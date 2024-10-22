3b84af384334f0f2abd86a9eb6eafa58b45748ec
Juan Alencar de Barros <127232363+juanalenca@users.noreply.github.com>
2024-10-21 21:32:17 -0300

    Update AppLayout.java

--- exercicios_javafx/src/layout/AppLayout.java
+++ exercicios_javafx/src/layout/AppLayout.java
@@ -9,7 +9,11 @@ public class AppLayout extends Application {
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		Parent raiz = null;
+		// O objeto 'raiz' será utilizado para armazenar o layout que será exibido na cena principal
+        	Parent raiz = null;
+        
+        	// Aqui você pode alternar entre diferentes gerenciadores de layout para testar suas funcionalidades.
+        	// Para isso, descomente a linha correspondente ao layout desejado.
 		
 		//raiz = new TesteAnchorPane();
 		//raiz = new TesteBorderPane();
@@ -18,11 +22,15 @@ public class AppLayout extends Application {
 		//raiz = new TesteStackPlane();
 		raiz = new TesteTilePane();
 		
-		Scene principal = new Scene(raiz, 800, 600);
-		
-		primaryStage.setScene(principal);
-		primaryStage.setTitle("Gerenciadores de Layout");
-		primaryStage.show();
+		// Define a cena principal com o layout selecionado, e define o tamanho da janela (800x600)
+        	Scene principal = new Scene(raiz, 800, 600);
+        
+	        // Configura a cena no palco principal e define o título da janela
+        	primaryStage.setScene(principal);
+        	primaryStage.setTitle("Gerenciadores de Layout");
+        
+        	// Exibe a janela da aplicação
+        	primaryStage.show();
 	}
 	
 	public static void main(String[] args) {
